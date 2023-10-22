521d8e0519edaae7097c0a71370bb7367d770802
Haoyu-Feng <haoyufeng199@gmail.com>
2023-10-22 11:28:15 +0800

    fix a typo in tests

--- src/tests/lab1-link-layer/utils.h
+++ src/tests/lab1-link-layer/utils.h
@@ -25,13 +25,13 @@ struct EthernetHeader
 /* The mac address of ns1 on my machine. */
 u_char ns1_mac[ETHER_ADDR_LEN] = {0x66, 0xcc, 0xc7, 0xf0, 0x49, 0x84};
 
-/* The mac address of ns1 on my machine. */
+/* The mac address of ns2 on my machine. */
 u_char ns2_mac[ETHER_ADDR_LEN] = {0x3e, 0x10, 0x9e, 0xf1, 0xcb, 0x3c};
 
 /* The mac address of ns3 on my machine. */
 u_char ns3_mac[ETHER_ADDR_LEN] = {0xea, 0x58, 0x7b, 0x6e, 0x46, 0xc3};
 
-/* The mac address of ns3 on my machine. */
+/* The mac address of ns4 on my machine. */
 u_char ns4_mac[ETHER_ADDR_LEN] = {0x86, 0x5f, 0x70, 0x13, 0x8a, 0x6c};
 
 /** 
